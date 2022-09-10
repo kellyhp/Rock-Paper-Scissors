e0d167af71c54a904c4e6fc171754dab886072a6
Kelly <107735247+kellyhp@users.noreply.github.com>
2022-09-09 17:06:35 -0700

    Update game.h
    reeedit function names

--- game.h
+++ game.h
@@ -2,8 +2,8 @@
 using namespace std;
 #ifndef ROCKPAPERSCISSORS_GAME_H
 #define ROCKPAPERSCISSORS_GAME_H
-int ConvertInput(string value);
-void play_game(int argc, char *argv[],string value, int rand_result, string start);
-int random_play(int argc, char* argv[]);
+string ConvertAI(int rand_play);
+void play_game(int argc, char *argv[],string value, string rand_result, string start);
+string random_play(int argc, char* argv[]);
 void replay_game(int argc, char *argv[], string exit, string start);
 #endif //ROCKPAPERSCISSORS_GAME_H
