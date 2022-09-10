ba0b3de302ded5d3714e51b78dd51d288df26246
Kelly <107735247+kellyhp@users.noreply.github.com>
2022-09-09 17:07:14 -0700

    Update InputValidation.cpp
    added new function RemoveChar

--- InputValidation.cpp
+++ InputValidation.cpp
@@ -3,8 +3,16 @@
 #include <algorithm>
 #include "InputValidation.h"
 
+string RemoveChar(string line) {
+    std::string chars = "()";
+    std::string s = line;
+    for (char i: chars) {
+        s.erase(std::remove(s.begin(), s.end(), i), s.end());
+    }
+    return s;
+}
 string ConvertLower(string line){
-    std::string new_line;
+    string new_line;
     std::for_each(line.begin(), line.end(), [](char & c){
         c = ::tolower(c);
     });
@@ -13,13 +21,13 @@ string ConvertLower(string line){
 }
 
 bool ValidInputs(string line) {
-    if (line == "r" || line == "rock" || line == "(r)ock") {
+    if (line == "r" || line == "rock") {
         return true;
     }
-    else if (line == "p" || line == "paper" || line =="(p)aper") {
+    else if (line == "p" || line == "paper") {
         return true;
     }
-    else if (line == "s" || line == "scissors" || line == "(s)cissors") {
+    else if (line == "s" || line == "scissors") {
         return true;
     }
     else {
@@ -28,10 +36,12 @@ bool ValidInputs(string line) {
 }
 
 bool ValidExit(string line) {
-    if (line == "y" || line == "yes" || line == "(y)es") {
+    if (line == "y" || line == "yes") {
+        line = "yes";
         return true;
     }
-    else if (line == "n" || line == "no" || line == "(n)o") {
+    else if (line == "n" || line == "no") {
+        line = "no";
         return true;
     }
     else {
@@ -42,26 +52,30 @@ bool ValidExit(string line) {
 string getValidChar(const std::string& prompt) {
     std::string lineRead;
     std::string line;
+    std::string new_line;
     bool readSuccessful = false;
     do{
         std::cout << prompt;
         std::getline(std::cin, lineRead); // grab the line
-        line = ConvertLower(lineRead);
-        readSuccessful = ValidInputs(line);
+        line = RemoveChar(lineRead);
+        new_line = ConvertLower(line);
+        readSuccessful = ValidInputs(new_line);
     }while(!readSuccessful); //keep asking until they give me what I want
-    return line;
+    return new_line;
 }
 
 string getValidChar2(const std::string& prompt, const std::string& replay) {
     std::string lineRead;
     std::string line;
+    std::string new_line;
     bool readSuccessful = false;
     do{
         std::cout << replay << endl;
         std::cout << prompt;
         std::getline(std::cin, lineRead); // grab the line
-        line = ConvertLower(lineRead);
-        readSuccessful = ValidExit(line);
+        line = RemoveChar(lineRead);
+        new_line = ConvertLower(line);
+        readSuccessful = ValidExit(new_line);
     }while(!readSuccessful); //keep asking until they give me what I want
-    return line;
+    return new_line;
 }
