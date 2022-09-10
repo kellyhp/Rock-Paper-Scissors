ba3e8703a13d62805675af57ebf44c3d2860dc2c
Kelly <107735247+kellyhp@users.noreply.github.com>
2022-09-09 17:06:09 -0700

    Update game.cpp
    added std::

--- game.cpp
+++ game.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include <string>
 #include <random>
-#include "game.h"
 #include <chrono>
+#include "game.h"
 #include "InputValidation.h"
 
-int ConvertInput(string value) {
-    int User_plays;
-    if (value == "r" || value == "rock" || value == "(r)ock") {
-        User_plays = 0;
+std::string ConvertAI(int rand_play) {
+    std::string AI_plays;
+    if (rand_play == 0) {
+        AI_plays = "rock";
     }
-    if (value == "p" || value == "paper" || value =="(p)aper") {
-        User_plays = 1;
+    if (rand_play == 1) {
+        AI_plays = "paper";
     }
-    if (value == "s" || value == "scissors" || value == "(s)cissors") {
-        User_plays = 2;
+    if(rand_play == 2) {
+        AI_plays = "scissors";
     }
-    return User_plays;
+    return AI_plays;
 }
 
-int random_play(int argc, char* argv[]){
+std::string random_play(int argc, char* argv[]){
     unsigned int seed;
     int play;
     if(argc == 1) {
@@ -31,73 +31,72 @@ int random_play(int argc, char* argv[]){
     std::minstd_rand generator(seed);
     std::uniform_int_distribution<int> range(0,2);
     play = range(generator);
-    return play;
+    std::string AI_plays = ConvertAI(play);
+    return AI_plays;
 }
 
-void play_game(int argc, char *argv[], string value, int rand_result, string start) {
-    string new_value = value;
-    int U_input = ConvertInput(new_value);
-    if (rand_result == 1 && U_input == 1) {
-        cout << "The ai played paper." << endl;
-        cout << "You and the AI both played paper." << endl;
-        cout << "Keep playing until someone wins." << endl;
-        string value1 = getValidChar(start);
-        int rand_result1 = random_play(argc, argv);
+void play_game(int argc, char *argv[], std::string value, std::string rand_result, std::string start) {
+    if (rand_result == "paper" && (value == "p" || value == "paper")) {
+        std::cout << "The ai played paper." << std::endl;
+        std::cout << "You and the AI both played paper." << std::endl;
+        std::cout << "Keep playing until someone wins." << std::endl;
+        std::string value1 = getValidChar(start);
+        std::string rand_result1 = random_play(argc, argv);
         play_game(argc, argv, value1, rand_result1, start);
     }
-    if (rand_result == 0 && U_input == 0) {
-        cout << "The ai played rock." << endl;
-        cout << "You and the AI both played rock." << endl;
-        cout << "Keep playing until someone wins." << endl;
-        string value1 = getValidChar(start);
-        int rand_result1 = random_play(argc, argv);
+    if (rand_result == "rock" && (value == "r" || value == "rock")) {
+        std::cout << "The ai played rock." << std::endl;
+        std::cout << "You and the AI both played rock." << std::endl;
+        std::cout << "Keep playing until someone wins." << std::endl;
+        std::string value1 = getValidChar(start);
+        std::string rand_result1 = random_play(argc, argv);
         play_game(argc, argv, value1, rand_result1, start);
     }
-    if (rand_result == 2 && U_input == 2) {
-        cout << "The ai played scissors." << endl;
-        cout << "You and the AI both played scissors." << endl;
-        cout << "Keep playing until someone wins." << endl;
-        string value1 = getValidChar(start);
-        int rand_result1 = random_play(argc, argv);
+    if (rand_result == "scissors" && (value == "s" || value == "scissors")) {
+        std::cout << "The ai played scissors." << std::endl;
+        std::cout << "You and the AI both played scissors." << std::endl;
+        std::cout << "Keep playing until someone wins." << std::endl;
+        std::string value1 = getValidChar(start);
+        std::string rand_result1 = random_play(argc, argv);
         play_game(argc, argv, value1, rand_result1, start);
     }
-    if (U_input == 1 && rand_result == 0) {
-        cout << "The ai played rock." << endl;
-        cout << "You win!" << endl;
+    if ((value == "paper" || value == "p") && rand_result == "rock") {
+        std::cout << "The ai played rock." << std::endl;
+        std::cout << "You win!" << std::endl;
     }
-    if (U_input == 1 && rand_result == 2) {
-        cout << "The ai played scissors." << endl;
-        cout << "The AI wins :(" << endl;
+    if ((value == "paper" || value == "p") && rand_result == "scissors") {
+        std::cout << "The ai played scissors." << std::endl;
+        std::cout << "The AI wins :(" << std::endl;
     }
-    if (U_input == 0 && rand_result == 2) {
-        cout << "The ai played scissors." << endl;
-        cout << "You win!" << endl;
+    if ((value == "rock" || value == "r") && rand_result == "scissors") {
+        std::cout << "The ai played scissors." << std::endl;
+        std::cout << "You win!" << std::endl;
     }
-    if (U_input == 0 && rand_result == 1) {
-        cout << "The ai played paper." << endl;
-        cout << "The AI wins :(" << endl;
+    if ((value == "rock" || value == "r") && rand_result == "paper") {
+        std::cout << "The ai played paper." << std::endl;
+        std::cout << "The AI wins :(" << std::endl;
     }
-    if (U_input == 2 && rand_result == 1) {
-        cout << "The ai played paper." << endl;
-        cout << "You win!" << endl;
+    if ((value == "scissors" || value == "s") && rand_result == "paper") {
+        std::cout << "The ai played paper." << std::endl;
+        std::cout << "You win!" << std::endl;
     }
-    if (U_input == 2 && rand_result == 0) {
-        cout << "The ai played rock." << endl;
-        cout << "The AI wins :(" << endl;
+    if ((value == "scissors" || value == "s") && rand_result == "rock") {
+        std::cout << "The ai played rock." << std::endl;
+        std::cout << "The AI wins :(" << std::endl;
     }
 }
 
-void replay_game(int argc, char *argv[], string leave, string start) {
-    if (leave == "yes" || leave == "y" || leave == "(y)es") {
-        string value = getValidChar(start);
-        int rand_result = random_play(argc, argv);
+void replay_game(int argc, char *argv[], std::string leave, std::string start) {
+    if (leave == "yes" || leave == "y") {
+        std::string value = getValidChar(start);
+        std::string rand_result = random_play(argc, argv);
         play_game(argc, argv, value, rand_result, start);
-        string replay = "Would you like to replay the game?";
-        string end = "Enter (Y)es or (N)o: ";
-        string leave1 = getValidChar2(end, replay);
+        std::string replay = "Would you like to replay the game?";
+        std::string end = "Enter (Y)es or (N)o: ";
+        std::string leave1 = getValidChar2(end, replay);
         replay_game(argc, argv, leave1, start);
     }
-    else if (leave == "no" || leave == "n" || leave == "(n)o") {
+    else if (leave == "no" || leave == "n") {
         _Exit(0);
     }
 
